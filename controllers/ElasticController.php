e04642bf55541d0548ad03f7c6238c0f2b55eb66
jimutsarder2019 <jimutsarder@gmail.com>
2023-09-06 00:13:19 +0600

    fix

--- controllers/ElasticController.php
+++ controllers/ElasticController.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/../api/vendor/autoload.php';
 use Yii;
 use yii\web\Controller;
 use yii\elasticsearch\Query;
+use app\components\ApplicationHelper;
 
 class ElasticController extends Controller
 {	
