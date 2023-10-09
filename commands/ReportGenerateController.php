4419b6bcf7540dbfd1af34255f62792e709205e3
jimutsarder2019 <jimutsarder@gmail.com>
2023-10-10 01:22:09 +0600

    fix

--- commands/ReportGenerateController.php
+++ commands/ReportGenerateController.php
@@ -1,8 +1,8 @@
 <?php
 namespace app\commands;
 
-ini_set('max_execution_time', '900');
-ini_set('memory_limit', '16384M');
+//ini_set('max_execution_time', '900');
+//ini_set('memory_limit', '16384M');
 
 require_once __DIR__ . '/../api/vendor/autoload.php';
 
@@ -24,12 +24,6 @@ class ReportGenerateController extends Controller
 	
 	public function actionProcess()
     {
-				/*$model = ReportBackup::findOne(['id' => 18]);
-				$model->report_type = 'test';
-				$model->save();
-				
-				
-				die;*/
 		$report_backup_list = $this->getReportBackupList();
 		ApplicationHelper::logger('Report generate process start...');
 		if(!empty($report_backup_list)){
