8157a33b249cfbfe81b7865cf764b6db303bbe8c
jimutsarder2019 <jimutsarder@gmail.com>
2023-10-17 23:23:46 +0600

    fix

--- commands/ReportGenerateController.php
+++ commands/ReportGenerateController.php
@@ -1,8 +1,8 @@
 <?php
 namespace app\commands;
 
-//ini_set('max_execution_time', '900');
-//ini_set('memory_limit', '16384M');
+ini_set('max_execution_time', '900');
+ini_set('memory_limit', '16384M');
 
 require_once __DIR__ . '/../api/vendor/autoload.php';
 
