f23956a18b2cc4aae24480e99df8c666898e14e9
jimutsarder2019 <jimutsarder@gmail.com>
2023-10-21 13:24:26 +0600

    update

--- commands/LogCheckController.php
+++ commands/LogCheckController.php
@@ -72,39 +72,8 @@ class LogCheckController extends Controller
 				}
 			}
 		}
-		self::send_mail('test', 'test_body', 'engrahuldeb@gmail.com');
 		ApplicationHelper::logger('End Checking router log...');
     }
-	
-	private function sendMail($missing_router, $to_email)
-	{
-		$license_data = CustomController::getLicenseData();
-		$post = [
-				'from_name'=>'Cloudhub',
-				'to_name'=>'Admin',
-				'to'=>$to_email,
-				'to_cc'=>'logreport@cloudhub.com.bd',
-				'from'=>'support@cloudhub.com.bd',
-				'message'=> "<p>Company Name: ".$license_data['registration_name']." </p> <p>License Number: ".$license_data['license_number']." </p> 
-				<p>Any log data didn't find in this router (".$missing_router.")</p>",
-				'subject'=> 'Log not found Alert',
-		];
-			
-		$url = 'https://www.travellersguru.com.bd/rest-api/send-alert-mail';
-		$ch = curl_init();
-		$params = http_build_query($post);
-		curl_setopt($ch, CURLOPT_URL,$url);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS,
-					$params);
-		
-		// Receive server response ...
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		
-		$server_output = curl_exec($ch);
-		
-		curl_close ($ch);
-	}
 	
 	
 	//Business partner registration using this mail function:
