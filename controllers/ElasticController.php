a83dc8777ebbc009bc5544889480356c6498262d
unknown <rahul@elifetransfer.com>
2024-04-28 17:19:16 +0600

    issue fix

--- controllers/ElasticController.php
+++ controllers/ElasticController.php
@@ -2,7 +2,7 @@
 //TODO:: CODE OPTIMIZE
 namespace app\controllers;
 
-ini_set('max_execution_time', '300');
+ini_set('max_execution_time', '89000');
 
 require_once __DIR__ . '/../api/vendor/autoload.php';
 
@@ -717,7 +717,7 @@ class ElasticController extends Controller
 		}
 		
 		if(!$limit){
-			$limit = 100;
+			$limit = 3;
 		}
 		$date_filter[] = [
 				"range"=>[
